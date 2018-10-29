617bc07cfbb96cfeb572efd65be64e5216a777e1
gronen <galitronen@gmail.com>
2018-10-29 18:39:17 -0400

    Initial Commit

---
+++ MapSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapSearch
+//
+//  Created by Galit Ronen on 10/29/18.
+//  Copyright © 2018 Consult Partners US. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
